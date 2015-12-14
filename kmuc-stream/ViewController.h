dc708c22ecb106e2c45c80b4e957cfd94508f93e
Nathan Lawrence <lawrencen@missouri.edu>
2015-12-14 10:28:09 -0600

    Initial Commit

---
+++ kmuc-stream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kmuc-stream
+//
+//  Created by Nathan Lawrence on 12/14/15.
+//  Copyright © 2015 Nathan Lawrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ kmuc-stream/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kmuc-stream
+//
+//  Created by Nathan Lawrence on 12/14/15.
+//  Copyright © 2015 Nathan Lawrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
